38d57251ebca0022953b0a222e8b28aa38dee9cc
“fidel” <cr.belga@gmail.com>
2019-08-14 22:52:03 -0400

    Inicializando projeto lista compra

---
+++ lista-compras/src/components/lista/Listas.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Menu from '../menu/Menu';
+class Listas extends Component {
+    render() {
+        return (
+            <div>
+                <Menu titulo="Minhas Listas"/>
+                Listas
+            </div>
+        );
+    }
+}
+
+export default Listas;
