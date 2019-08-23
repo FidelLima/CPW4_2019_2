9b350df3571d518b97cf6751a1791a7115b0fc1b
“fidel” <cr.belga@gmail.com>
2019-08-22 20:59:40 -0400

    Carregando as listas na página inicial

--- lista-compras/src/components/lista/Listas.js
+++ lista-compras/src/components/lista/Listas.js
@@ -1,17 +1,52 @@
 import React, { Component } from 'react';
 import Menu from '../menu/Menu';
-import logo from'../../img/logo.png';
+import logo from '../../img/logo.png';
+import ListaService from '../../services/ListaService';
 
 class Listas extends Component {
+
+    constructor() {
+        // toda vez que criar o constructor lembrar de invocar o construtor da classe component. mãe component na primeira linha de cod
+
+        super();
+
+        this.state = {
+            listas: []
+        }
+        this.service = new ListaService();
+    }
+
+    componentDidMount() {
+        const listas =
+            this.service.recuperarListas();
+        this.setState({ listas });
+    }
+
+
     render() {
+        const listas =
+            this.state.listas.map(lista => (
+                <li key={lista._id}>{lista.nome}</li>
+            ));
+
         return (
             <div>
                 <Menu
-                logo ={logo}
-                paginaAnterior="/"
-                titulo="Minhas Listas"/>
-                Listas
-                
+                    logo={logo}
+                    paginaAnterior="/"
+                    titulo="Minhas Listas" />
+                <div className="container">
+                    <div>
+                        <h2>Minhas Listas</h2>
+
+                        <ul>
+                            {listas}
+
+                        </ul>
+                    </div>
+
+                </div>
+
             </div>
         );
     }
